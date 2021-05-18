dbf7d5197b378e0413f38295fac495026f2d1275
Yasmin002 <alvesyasmin094@gmail.com>
2021-05-19 04:39:43 +0900

    Projeto Terminado

---
+++ www/components/plugins.js
@@ -0,0 +1,23 @@
+// This is a JavaScript file
+function mostraMapa(lat, long){
+  L.mapquest.key = 'C0AXWURTT9zPGAZo3yWGZIUgtO1hu4KC';
+
+        var map = L.mapquest.map('map', {
+        center: [lat, long],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 15
+        });
+        map.addControl(L.mapquest.control());
+}
+
+$(document).on("click", "#local", function(){
+ var onSuccess = function(position) {
+   mostraMapa(position.coords.latitude, position.coords.longitude)
+    };
+
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+});
